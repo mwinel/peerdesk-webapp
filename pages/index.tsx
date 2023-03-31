ed04c79e2349b0490e043cf4f23431ad3ea6763a
Nelson M <mwinel@live.com>
2023-03-31 18:56:31 +0300

    Remove google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import Button from '@/components/Button'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
